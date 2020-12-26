0fbf1f7130f9000d1123f9d3b56371607624019c
Катерина Майстер <kateryna.maister@nure.ua>
2020-12-26 16:07:19 +0200

    +++

---
+++ Katya Maister.cpp
@@ -0,0 +1,9 @@
+﻿// Katya Maister.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Wo
+}
