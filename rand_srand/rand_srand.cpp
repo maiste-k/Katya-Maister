4521ab4a46172cc9f3ff59813751b4146dd6e6e6
Катерина Майстер <kateryna.maister@nure.ua>
2021-02-18 11:55:52 +0200

    +

---
+++ rand_srand/rand_srand.cpp
@@ -0,0 +1,46 @@
+﻿// rand_srand.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+/*Заполнить массив случайными числами. Генератор уникальных случайных чисел. rand/srand */
+
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	srand(time(NULL));
+
+	int const SIZE = 10;
+	int arr[SIZE];
+
+
+	bool hasAlready;
+
+	for (int i = 0; i < SIZE;)
+	{
+		hasAlready = false;
+		int newRandomValue = rand() % 20;
+
+		for (int j = 0; j < i; j++)
+		{
+
+			if (arr[j] = newRandomValue)
+			{
+				hasAlready = true;
+				break;
+			}
+		}
+		if (!hasAlready)
+		{
+			arr[i] = newRandomValue;
+			i++;
+		}
+
+
+	}
+
+}
